896da9b6b0f21ec8df36d01ed4d065faeae6b9cb
Diego-Sant <diego_santana@unigranrio.br>
2022-07-17 22:43:54 -0300

    Updates

---
+++ precache-manifest.56e1e78dd863bfee19400ed2d323d0f4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "914382ac2be374effaed",
+    "url": "/static/css/main.405412e0.chunk.css"
+  },
+  {
+    "revision": "914382ac2be374effaed",
+    "url": "/static/js/main.914382ac.chunk.js"
+  },
+  {
+    "revision": "f525a3d94476e8a55a26",
+    "url": "/static/js/1.f525a3d9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "40643a10df0b03fa811a2f469943b08e",
+    "url": "/static/media/visualizacao-black.40643a10.svg"
+  },
+  {
+    "revision": "a241db22900a24c5bb6fcf82b20048ce",
+    "url": "/static/media/usuario.a241db22.svg"
+  },
+  {
+    "revision": "4ce15ed8e41974f50628587af3b32c4b",
+    "url": "/static/media/visualizacao.4ce15ed8.svg"
+  },
+  {
+    "revision": "c44274f490de188515b64b5793c7841b",
+    "url": "/static/media/login.c44274f4.jpg"
+  },
+  {
+    "revision": "384f72b3014ca9dcb02a5189ec9455a6",
+    "url": "/static/media/fla-footer.384f72b3.svg"
+  },
+  {
+    "revision": "d89b851ef0d9842a17749a300910fd9c",
+    "url": "/static/media/fla.d89b851e.svg"
+  },
+  {
+    "revision": "446e77189d39a9a9a5e3bba849225d2f",
+    "url": "/static/media/enviar.446e7718.svg"
+  },
+  {
+    "revision": "1b704d1cf0245bcf9b280dae5ea8ea5a",
+    "url": "/static/media/feed.1b704d1c.svg"
+  },
+  {
+    "revision": "1acc1113ac954fcb5237e220ca0a86e9",
+    "url": "/static/media/estatisticas.1acc1113.svg"
+  },
+  {
+    "revision": "482bb4bdba52e54f0b3b834d46fcf641",
+    "url": "/static/media/adicionar.482bb4bd.svg"
+  },
+  {
+    "revision": "b648ffb0be1509f61d7eec7edd484d11",
+    "url": "/static/media/sair.b648ffb0.svg"
+  },
+  {
+    "revision": "9aadfc6654dce07fddcbba71e1772245",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/WallpaperFLA/precache-manifest.bd857e0d59ee3a5e5c81ea8e5e3a6fde.js"
+  "/precache-manifest.56e1e78dd863bfee19400ed2d323d0f4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/WallpaperFLA/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
